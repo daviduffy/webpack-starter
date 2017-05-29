37cbe4b6f86a534b4d6f71cf1ffefcff16db05ae
David Duffy <daviduffy1@gmail.com>
2017-05-29 14:05:48 -0700

    completed most of the static components for first and second slides

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -12,6 +12,8 @@ const app = [
     leftContentClasses: '',
     rightContentClasses: 'mosaic-container',
     id: 'card_h',
+    fa_icon: 'fa-home',
+    curtain: false,
     contentMeta: [
       {
         type: 'image',
@@ -47,10 +49,12 @@ const app = [
     leftContentClasses: '',
     rightContentClasses: '',
     id: 'card_1',
+    fa_icon: 'fa-github',
+    curtain: true,
     contentMeta: [
       {
         type: 'image',
-        src: {cat_1},
+        src: `${cat_1}`,
         alt: 'a cat image again',
         classes: ''
       }
@@ -60,9 +64,38 @@ const app = [
 
 
 const App = () => {
+
+  const changePage = () => {
+    console.log(this);
+
+    if ( this.classList.contains('controls__link--active') ) {
+      console.log('break');
+      return;
+    }
+    console.log('change');
+    let cardPrev = document.getElementsByClassName('card--previous');
+    console.log(`click ${this}`);
+    
+    let cardCurr = document.querySelector('.card--active');
+    let buttonCurr = document.getElementsByClassName('controls__link--active')[0];
+    
+    if ( !!cardPrev ) {
+      cardPrev[0].classList.remove('card--previous');
+    }
+    
+    cardCurr.classList.add('card--previous');
+    cardCurr.classList.remove('card--active');
+    buttonCurr.classList.remove('controls__link--active');
+    
+    let cardNextIndex = this.getAttribute('data-index');
+    let cardNext = document.getElementById(cardNextIndex);
+    cardNext.classList.add('card--active');
+    this.classList.add('controls__link--active');
+  }
+
   return (
     <div className="card-container">
-      {app.map((current) => {
+      {app.map((current, index) => {
         return (
           <Page
             type={current.type}
@@ -72,45 +105,95 @@ const App = () => {
             rightContentClasses={current.rightContentClasses}
             leftContentClasses={current.leftContentClases}
             id={current.id}
-            contentMeta={current.contentMeta} />
-          );}
-        )}
+            contentMeta={current.contentMeta}
+            curtain={current.curtain}
+            key={index}  />
+          );
+        })
+      }
+      <TopControls />
+      <div className='controls controls--bottom'>
+        {app.map((current, index) => {
+          return (
+            <BottomButton
+              id={current.id}
+              fa_icon={current.fa_icon}
+              pageChangeHandler={changePage}
+              key={index} />
+            );
+          })
+        }
+      </div>
     </div>
   )
 }
 
 const Page = (props) => {
-  const type = props.type;
   return (
-    <div className={props.containerClasses}>
+    <div className={props.containerClasses} >
       <div className={props.leftContentClasses ? `${props.leftContentClasses} card__content-left` : 'nope card__content-left'}>
         <h2>{props.title[0]}<strong>{props.title[1]}</strong></h2>
         <p>{props.subtitle}</p>
       </div>
       <div className={props.rightContentClasses ? `${props.rightContentClasses} card__content-right` : 'nope card__content-right'}>
-        {
-          props.contentMeta.map((current) => {
-          return (
-            <Card
-              type={current.type}
-              src={current.src}
-              alt={current.alt}
-              classes={current.classes} />
-            );}
-        )}
+        <div className="card__content-right-wrapper">
+          {
+            props.contentMeta.map((current, index) => {
+            return (
+              <Card
+                type={current.type}
+                src={current.src}
+                alt={current.alt}
+                classes={current.classes}
+                key={index} />
+              );
+            })
+          }
+        </div>
       </div>
+      <div className="card__bg"></div>
+      { 
+        props.curtain &&
+        <div className="curtain"></div>
+      }
     </div>
   )
 }
 
 const Card = (props) => {
   return (
-    <div className={props.classes}>
+    <div className={props.classes} >
       {
         props.type === 'image' &&
         <img src={props.src} alt={props.alt} />
       }
     </div>
+  )
+}
+
+const TopControls = () => {
+  return (
+    <div className='controls controls--top'>
+      <a className='controls__link controls__link--top controls__link--active' data-index='card_h' href='javascript:void(0)' id='home_card_trigger'>
+        <svg height='30' viewBox='0 0 30 30' width='40' xmlns='http://www.w3.org/2000/svg'>
+          <polyline fill='none' points='15 5 5 15 15 25' strokeLinecap='butt' strokeLinejoin='miter' strokeWidth='3' stroke='white'></polyline>
+          <polyline fill='none' points='5 15 35 15' strokeLinecap='butt' strokeLinejoin='miter' strokeWidth='3' stroke='white'></polyline>
+        </svg>
+      </a>
+      <a className='controls__link controls__link--top' href='javascript:void(0)' id='close_trigger'>
+        <svg height='30' viewBox='0 0 30 30' width='30' xmlns='http://www.w3.org/2000/svg'>
+          <polyline fill='none' points='5 5 25 25' strokeLinecap='butt' strokeLinejoin='miter' strokeWidth='3' stroke='white'></polyline>
+          <polyline fill='none' points='5 25 25 5' strokeLinecap='butt' strokeLinejoin='miter' strokeWidth='3' stroke='white'></polyline>
+        </svg>
+      </a>
+    </div>
+  )
+}
+const BottomButton = (props) => {
+  return (
+      <a className='controls__link controls__link--bottom' data-index={props.id} href='javascript:void(0)' id={`${props.id}_trigger`} onClick={props.pageChangeHandler}>
+        <i aria-hidden='true' className={`fa ${props.fa_icon}`}></i>
+      </a>
   )
 }
 
--- src/input.js
+++ src/input.js
@@ -5,4 +5,7 @@ import ReactDOM from 'react-dom';
 import { render } from 'react-dom'
 import App from './app/App.jsx';
 
+
 ReactDOM.render(<App />, document.getElementById('root'));
+
+// import init from './app/init.js';
